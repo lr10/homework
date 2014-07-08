a699e385532265b29ebeeae1211c99f7e39c12bc
Leander Rodriguez <lr10@me.com>
2014-07-08 13:44:37 -0700

    Removed previously added TextField and TextView as I realized it was not what the assignment was.  Instead created an array of strings, along with a new TextView to display a random string upon onCreate.

--- homework2/src/edu/oit/cst407/hw2/StateActivity.java
+++ homework2/src/edu/oit/cst407/hw2/StateActivity.java
@@ -1,15 +1,40 @@
 package edu.oit.cst407.hw2;
 
+import java.util.Random;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class StateActivity extends Activity {
 
+	String [] m_planetsArray;
+	TextView m_textView;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_state);
+		
+		m_planetsArray =   getResources().getStringArray(R.array.planets_array); 
+		updateTextView();
 	}
+	
+	
+	@Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    private void updateTextView() {
+        TextView m_textView = (TextView)findViewById(R.id.randomTextView); 
+        Random random = new Random();
+
+        int maxIndex = m_planetsArray.length;
+        int generatedIndex = random.nextInt(maxIndex);
+
+        m_textView.setText(m_planetsArray[generatedIndex]);   
+    }
 }
