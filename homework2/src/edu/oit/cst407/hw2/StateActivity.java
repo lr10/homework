fa99018ac42c062b435164674f7ee7156c14a484
Leander Rodriguez <lr10@me.com>
2014-07-08 11:57:21 -0700

    Added new State Activity and respective layout.

---
+++ homework2/src/edu/oit/cst407/hw2/StateActivity.java
@@ -0,0 +1,15 @@
+package edu.oit.cst407.hw2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class StateActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_state);
+	}
+}
