027fb2ba995548a870ead5926b2c7e64e2df17f4
Leander Rodriguez <lr10@me.com>
2014-07-10 13:06:33 -0700

    Created a new Android Project in homework3 folder.  Project contains new GenieActivity and genie layout.

---
+++ homework3/src/edu/oit/cst407/hw3/GenieActivity.java
@@ -0,0 +1,13 @@
+package edu.oit.cst407.hw3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GenieActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_genie);
+	}
+}
