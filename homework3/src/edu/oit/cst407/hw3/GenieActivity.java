ff37e96fd02213164aab712ef8fd7ea26e9a7880
Leander Rodriguez <lr10@me.com>
2014-07-12 19:19:43 -0700

    Refactored code b/c I noticed that the methods that needed to be adjusted were onPause and onCreate, and not onSaveInstance.  Imported SharedPreferences library and added SharedPreferences functionality to both onPause() and onResume() methods.

--- homework3/src/edu/oit/cst407/hw3/GenieActivity.java
+++ homework3/src/edu/oit/cst407/hw3/GenieActivity.java
@@ -1,20 +1,26 @@
 package edu.oit.cst407.hw3;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class GenieActivity<MainActivity> extends Activity {
+public class GenieActivity extends Activity {
 
 	private static final String TAG = "SavePreferences: ";
-	private Button b1 = null;
-	private Button b2 = null;
-	private Button b3 = null;
-	private Boolean boolButtonClicked = false;
-	private static final String BUTTON_CLICKED = "Button_Clicked";
+	private Button[] buttons = new Button[3];
+	private boolean [] boolButtonsClicked = new boolean[3];
+	
+	private SharedPreferences prefs;
+	private String prefName = "MyPref";
+	private static final String BUTTON1_STATE = "button1_State";
+	private static final String BUTTON2_STATE = "button2_State";
+	private static final String BUTTON3_STATE = "button3_State";
+	
+	public int idx;
 	
 
 	@Override
@@ -23,31 +29,58 @@ public class GenieActivity<MainActivity> extends Activity {
 		setContentView(R.layout.activity_genie);
 		Log.v(TAG, "onCreate()");
 		
-		b1 = (Button)findViewById(R.id.button1);
-		b2 = (Button)findViewById(R.id.button2);
-		b3 = (Button)findViewById(R.id.button3);
+		buttons[0] = (Button)findViewById(R.id.button1);
+		buttons[1] = (Button)findViewById(R.id.button2);
+		buttons[2] = (Button)findViewById(R.id.button3);
 		
-		buttonClicked();
-		
-		/*
-		b1.setOnClickListener(new OnClickListener() {
+		buttons[0].setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				((Button)v).setEnabled(false);
+				boolButtonsClicked[0] = true;
 			}
 		});
-		b2.setOnClickListener(new OnClickListener() {
+		buttons[1].setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				((Button)v).setEnabled(false);
+				boolButtonsClicked[1] = true;
 			}
 		});
-		b3.setOnClickListener(new OnClickListener() {
+		buttons[2].setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				((Button)v).setEnabled(false);
+				boolButtonsClicked[2] = true;
 			}
-		});*/
+		});
+	}
+	
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		Log.v(TAG, "onPause()");
+		
+		for( idx = 0; idx < buttons.length; ++idx)
+		{
+			if( !buttons[idx].isEnabled() )
+			{
+				boolButtonsClicked[idx] = true;
+			}
+			else
+			{
+				boolButtonsClicked[idx] = false;
+			}
+		}
+		
+		
+		prefs = getSharedPreferences(prefName, MODE_PRIVATE);
+		SharedPreferences.Editor editor = prefs.edit();
+		editor.putBoolean(BUTTON1_STATE, boolButtonsClicked[0]);
+		editor.putBoolean(BUTTON2_STATE, boolButtonsClicked[1]);
+		editor.putBoolean(BUTTON3_STATE, boolButtonsClicked[2]);
+		editor.commit();
 	}
 
 	@Override
@@ -55,57 +88,44 @@ public class GenieActivity<MainActivity> extends Activity {
 		// TODO Auto-generated method stub
 		super.onResume();
 		Log.v(TAG, "onResume()");
+		
+		prefs = getSharedPreferences(prefName, MODE_PRIVATE);
+		boolButtonsClicked[0] = prefs.getBoolean(BUTTON1_STATE, false);
+		boolButtonsClicked[1] = prefs.getBoolean(BUTTON2_STATE, false);
+		boolButtonsClicked[2] = prefs.getBoolean(BUTTON3_STATE, false);
+		
+		for( idx = 0; idx < boolButtonsClicked.length; ++idx)
+		{
+			if( boolButtonsClicked[idx] == true )
+			{
+				buttons[idx].setEnabled(false);
+			}
+			else
+			{
+				buttons[idx].setEnabled(true);
+			}
+		}
+	}
+
+	
+	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+		Log.v(TAG, "onStart()");
 	}
 
 	@Override
-	protected void onPause() {
+	protected void onStop() {
 		// TODO Auto-generated method stub
-		super.onPause();
-		Log.v(TAG, "onPause()");
+		super.onStop();
+		Log.v(TAG, "onStop()");
 	}
-	
+
 	@Override
-	protected void onSaveInstanceState(Bundle outState) {
+	protected void onDestroy() {
 		// TODO Auto-generated method stub
-		super.onSaveInstanceState(outState);
-		Log.v(TAG, "onSaveInstanceState()");
-		
-		outState.putBoolean(BUTTON_CLICKED, boolButtonClicked);
-	}
-	
-	@Override
-	protected void onRestoreInstanceState(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
-		super.onRestoreInstanceState(savedInstanceState);
-		Log.v(TAG, "onRestoreInstanceState()");
-		
-		if( savedInstanceState != null && savedInstanceState.containsKey(BUTTON_CLICKED))
-		{
-			boolButtonClicked = savedInstanceState.getBoolean(BUTTON_CLICKED);
-		}
-		
-		buttonClicked();
-	}
-	
-	private void buttonClicked() {
-		
-		b1.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				((Button)v).setEnabled(false);
-			}
-		});
-		b2.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				((Button)v).setEnabled(false);
-			}
-		});
-		b3.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				((Button)v).setEnabled(false);
-			}
-		});
+		super.onDestroy();
+		Log.v(TAG, "onDestroy()");
 	}
 }
