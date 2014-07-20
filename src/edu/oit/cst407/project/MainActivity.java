1624b0bf9fc9e31dcd552313f03b1413c13e3090
Leander Rodriguez <lr10@me.com>
2014-07-19 20:42:34 -0700

    Added an settings icon in the app's action bar.

--- src/edu/oit/cst407/project/MainActivity.java
+++ src/edu/oit/cst407/project/MainActivity.java
@@ -2,6 +2,8 @@ package edu.oit.cst407.project;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu( Menu menu ) {
+		MenuInflater mif = getMenuInflater();
+		mif.inflate(R.menu.activity_main_action, menu);
+		return super.onCreateOptionsMenu(menu);
 	}
 }
