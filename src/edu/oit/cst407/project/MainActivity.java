6a40c31158f8b933e61bd22b7f8c6f4692e4a916
Leander Rodriguez <lr10@me.com>
2014-07-21 14:45:57 -0700

    Changed app name and app icon.  Removed an icon from the action bar.  Added setMyLocationEnabled icon to map.e

--- src/edu/oit/cst407/project/MainActivity.java
+++ src/edu/oit/cst407/project/MainActivity.java
@@ -4,13 +4,24 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
 public class MainActivity extends Activity {
 
+	private GoogleMap map;
+	private final LatLng LOCATION_CURRENT = new LatLng(51.5033630,-0.1276250);
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+		map.setMyLocationEnabled(true);
 	}
 	
 	@Override
@@ -19,4 +30,22 @@ public class MainActivity extends Activity {
 		mif.inflate(R.menu.activity_main_action, menu);
 		return super.onCreateOptionsMenu(menu);
 	}
+	
+	@Override
+	public boolean onOptionsItemSelected( MenuItem item ) {
+		
+		switch(item.getItemId())
+		{
+			case R.id.action_newgame :
+				
+				return true;
+			
+			case R.id.action_settings :
+				 
+				return true;
+		}
+		
+		return super.onOptionsItemSelected(item);
+	}
+	
 }
